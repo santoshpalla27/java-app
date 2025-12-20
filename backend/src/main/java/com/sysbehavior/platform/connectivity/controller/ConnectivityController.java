b27c26f231a9a26804ce627508545d4e9bc05449
santoshpalla27 <santoshpalla2002@gmail.com>
2025-12-20 22:33:49 +0530

    fix: add missing @PathVariable annotation to ConnectivityController

--- backend/src/main/java/com/sysbehavior/platform/connectivity/controller/ConnectivityController.java
+++ backend/src/main/java/com/sysbehavior/platform/connectivity/controller/ConnectivityController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -81,7 +82,7 @@ public class ConnectivityController {
      * @return Connection snapshot
      */
     @GetMapping("/{type}")
-    public ResponseEntity<Map<String, Object>> getDependencyStatus(String type) {
+    public ResponseEntity<Map<String, Object>> getDependencyStatus(@PathVariable String type) {
         try {
             DependencyType dependencyType = DependencyType.valueOf(type.toUpperCase());
             ConnectionSnapshot snapshot = registry.getSnapshot(dependencyType);
