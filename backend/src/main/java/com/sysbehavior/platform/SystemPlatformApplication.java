2b6c28fcd9f06a80302488afef234b332b3f95b1
santoshpalla27 <santoshpalla2002@gmail.com>
2025-12-20 21:56:05 +0530

    script

--- backend/src/main/java/com/sysbehavior/platform/SystemPlatformApplication.java
+++ backend/src/main/java/com/sysbehavior/platform/SystemPlatformApplication.java
@@ -2,10 +2,12 @@ package com.sysbehavior.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@ComponentScan(basePackages = {"com.sysbehavior.platform", "com.platform"})
 public class SystemPlatformApplication {
 
 	public static void main(String[] args) {
